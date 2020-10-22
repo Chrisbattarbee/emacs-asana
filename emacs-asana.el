d3510d58f21735502ab73e28545710f87a22f30f
Chris Battarbee <cbattarbee@palantir.com>
2020-10-22 13:31:34 +0100

    Init

---
+++ emacs-asana.el
@@ -0,0 +1,18 @@
+;;; emacs-asana.el --- Interface with Asana via org mode and agenda
+
+;; Author: Chris Battarbee <chris@chrisbattarbee.com>
+;; Version: 0.1
+;; Package-eqires: ((request "0.3.2"))
+;; Keywords: multimedia, hypermedia
+;; URL: https://github.com/Chrisbattarbee/emacs-asana
+
+;;; Commentary:
+
+;; This package provides an interface between emacs org mode and asana.com.
+;; It allows you to import tasks from asana and export org mode tasks to asana.
+
+
+;;;###autoload
+
+
+
